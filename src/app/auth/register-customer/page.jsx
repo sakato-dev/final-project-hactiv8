9079bf850ae822444af5b8d080fb531ac404eacc
kevinsultana <kevinpdg.ks@gmail.com>
2025-08-23 13:29:43 +0700

    feat: initial route for project

--- src/app/auth/register-customer/page.jsx
+++ src/app/auth/register-customer/page.jsx
@@ -4,18 +4,20 @@ export default function RegisterCustomer() {
   return (
     <div>
       RegisterCustomer
-      <Link href="/auth/login" className="p-2 bg-blue-500 text-white">
-        login
-      </Link>
-      <Link href="/auth/register" className="p-2 bg-blue-500 text-white">
-        Home Admin
-      </Link>
-      <Link href="/auth/register" className="p-2 bg-blue-500 text-white">
-        Home kasir
-      </Link>
-      <Link href="/auth/register" className="p-2 bg-blue-500 text-white">
-        Home customer
-      </Link>
+      <div className="flex gap-4">
+        <Link href="/auth/login" className="p-2 bg-blue-500 text-white">
+          login
+        </Link>
+        <Link href="/auth/register" className="p-2 bg-blue-500 text-white">
+          Home Admin
+        </Link>
+        <Link href="/auth/register" className="p-2 bg-blue-500 text-white">
+          Home kasir
+        </Link>
+        <Link href="/auth/register" className="p-2 bg-blue-500 text-white">
+          Home customer
+        </Link>
+      </div>
     </div>
   );
 }
